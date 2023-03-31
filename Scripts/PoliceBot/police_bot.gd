4166c0de7d0028f937f926a644ce853f5398cf07
JojoMakesGames <jojo.oatey@gmail.com>
2023-03-30 22:42:27 -0500

    also fix for robopolice boi

--- Scripts/PoliceBot/police_bot.gd
+++ Scripts/PoliceBot/police_bot.gd
@@ -5,6 +5,7 @@ const SPEED = 1.0
 const JUMP_VELOCITY = 4.5
 var hacked: bool
 @onready var camera_placement: Node3D = $CameraPlacement
+var input_direction: Vector3
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
