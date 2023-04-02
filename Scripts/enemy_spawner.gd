24a08a8695878148a683834b20f555c39c29842a
JojoMakesGames <jojo.oatey@gmail.com>
2023-04-01 22:15:08 -0500

    Add teleporter sound

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -2,8 +2,10 @@ extends Node3D
 
 var enemy: PackedScene = preload("res://Scenes/PoliceBot.tscn")
 @onready var spawn_location: Node3D = $Spawn_Location
+@onready var audio: AudioStreamPlayer3D = $AudioStreamPlayer3D
 
 func _on_timer_timeout():
+	audio.play()
 	var instance = enemy.instantiate()
 	instance.global_position = spawn_location.global_position
 	var root = get_tree().get_root()
