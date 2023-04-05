dcf36a354963342191f93ff0b1357395809e6aba
JojoMakesGames <jojo.oatey@gmail.com>
2023-04-05 17:15:07 -0400

    queue free

--- Scenes/Assets/UI/Lose.gd
+++ Scenes/Assets/UI/Lose.gd
@@ -2,8 +2,10 @@ extends Control
 
 
 func _on_button_pressed():
+	get_tree().call_group("enemy_bots", "queue_free")
 	get_tree().change_scene_to_file("res://Scenes/Tests/Test_Factory.tscn")
 
 
 func _on_return_to_title_pressed():
+	get_tree().call_group("enemy_bots", "queue_free")
 	get_tree().change_scene_to_file("res://Scenes/Levels/TitleScreen.tscn")
